b8d58fde9f05a7c832c0ab6263c0323964bf29db
Fernando Duro <main@fernandoduro.com.br>
2022-02-09 17:21:33 -0300

    First

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+function Main(props) {
+    return <>
+        <div>1x{props.number}={1*props.number}</div>
+        <div>2x{props.number}={2*props.number}</div>
+        <div>3x{props.number}={3*props.number}</div>
+        <div>4x{props.number}={4*props.number}</div>
+        <div>5x{props.number}={5*props.number}</div>
+        <div>6x{props.number}={6*props.number}</div>
+        <div>7x{props.number}={7*props.number}</div>
+        <div>8x{props.number}={8*props.number}</div>
+        <div>9x{props.number}={9*props.number}</div>
+        <div>10x{props.number}={10*props.number}</div>
+    </>
+}
+
+export default Main
